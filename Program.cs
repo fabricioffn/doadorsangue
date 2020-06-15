e458598c54974279fa7826cf4e90b7e30e78bc3e
fabricio <fnmamao@gmail.com>
2020-06-15 11:45:35 -0300

    exercicio

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace doadorsangue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            Console.WriteLine("digite sua idade");
+            idade = Convert.ToInt16(Console.ReadLine());
+
+            if(idade >= 18 &  idade <= 67)
+            {
+                Console.WriteLine("você pode ser doador");
+            }
+           else
+           {
+               Console.WriteLine("você não pode ser doador");
+           }
+        }
+    }
+}
